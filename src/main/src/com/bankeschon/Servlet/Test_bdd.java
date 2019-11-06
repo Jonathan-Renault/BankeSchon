d329e4ab7d549e734a469a761cd65479cb13abbc
Dorian Legros <d.ratha76@hotmail.com>
2019-11-06 10:01:45 +0100

    test connection bdd, 2ème essai

--- src/main/src/com/bankeschon/Servlet/Test_bdd.java
+++ src/main/src/com/bankeschon/Servlet/Test_bdd.java
@@ -1,6 +1,8 @@
 package com.bankeschon.Servlet;
 
+import com.bankeschon.Models.User;
 import com.bankeschon.Utils.Database;
+import com.bankeschon.Utils.Filter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,15 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "Test_bdd")
 public class Test_bdd extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            Database.connect();
-            Database.close();
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        User myuser = new User();
+        ArrayList<String> fields = new ArrayList<>();
+        ArrayList<String> filters = new ArrayList<>();
+        fields.add("*");
+        Database.select(myuser, fields, filters);
     }
 }
