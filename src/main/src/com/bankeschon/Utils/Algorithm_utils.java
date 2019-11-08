0992303f953621d4df8573a00fe5af344a272026
Dorian Legros <d.ratha76@hotmail.com>
2019-11-08 12:08:27 +0100

    patch de la branche

--- src/main/src/com/bankeschon/Utils/Algorithm_utils.java
+++ src/main/src/com/bankeschon/Utils/Algorithm_utils.java
@@ -1,13 +1,13 @@
 package com.bankeschon.Utils;
 
 import com.bankeschon.Models.Prospect;
-import com.bankeschon.Models.
+import com.bankeschon.Models.BaseModelORM;
 import com.bankeschon.Utils.Database;
 
 import java.util.ArrayList;
 
 public class Algorithm_utils {
-    public Prospect lastProspectModified (Integer id) {
+    /*public Prospect lastProspectModified (Integer id) {
         BaseModelORM _object = new BaseModelORM();
         Prospect prospect = new Prospect();
         ArrayList<String> fields = new ArrayList<String>();
@@ -16,5 +16,5 @@ public class Algorithm_utils {
         filter.add(Integer.toString(id));
         prospect = Database.select(_object, fields, filter);
         return prospect;
-    }
+    }*/
 }
