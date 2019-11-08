1b0307f28788969691908ba72ef710ccd5d5e612
Dorian Legros <d.ratha76@hotmail.com>
2019-11-08 14:51:35 +0100

    création de méthodes dans le Util de l'algo, à tester

--- src/main/src/com/bankeschon/Utils/Algorithm_utils.java
+++ src/main/src/com/bankeschon/Utils/Algorithm_utils.java
@@ -1,20 +1,35 @@
 package com.bankeschon.Utils;
 
-import com.bankeschon.Models.Prospect;
 import com.bankeschon.Models.BaseModelORM;
-import com.bankeschon.Utils.Database;
+import com.bankeschon.Models.Prospect;
+import com.bankeschon.Models.Offer_history;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Algorithm_utils {
-    /*public Prospect lastProspectModified (Integer id) {
-        BaseModelORM _object = new BaseModelORM();
-        Prospect prospect = new Prospect();
+    public List lastProspectModified (Integer id) {
+        BaseModelORM _object = new Prospect();
+        _object.getTableName();
+        List prospect;
         ArrayList<String> fields = new ArrayList<String>();
         ArrayList<String> filter = new ArrayList<String>();
         fields.add("*");
-        filter.add(Integer.toString(id));
+        filter.add("id =" + id);
         prospect = Database.select(_object, fields, filter);
         return prospect;
-    }*/
+    }
+
+    public boolean checkOffersProposed (Integer prospect_id, String offer_id) {
+        List result;
+        BaseModelORM _object = new Offer_history();
+        _object.getTableName();
+        ArrayList<String> fields = new ArrayList<String>();
+        ArrayList<String> filter = new ArrayList<String>();
+        fields.add("accepted");
+        filter.add("id_prospect =" + prospect_id + "AND accepted = true AND id_offer =" + offer_id);
+        result = Database.select(_object, fields, filter);
+
+        return result.isEmpty();
+    }
 }
